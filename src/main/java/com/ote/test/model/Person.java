ce288f0a930c878335ba2715452da0ba3ddcc4d6
Olivier TERRIEN <oterrien@neuf.fr>
2016-11-14 12:42:18 +0100

    Update Person.java

--- src/main/java/com/ote/test/model/Person.java
+++ src/main/java/com/ote/test/model/Person.java
@@ -3,9 +3,17 @@ package com.ote.test.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.HibernateException;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.IdentifierGenerator;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Entity
 @Table(name = "PERSON_INFO")
@@ -15,10 +23,10 @@ public class Person implements IEntity<Person.Key> {
 
     public static final String GENERATOR_NAME = "seq_id";
 
-    //@GenericGenerator(name = GENERATOR_NAME, strategy = Key.GENERATOR_CLASS)
-    //@GeneratedValue(generator = GENERATOR_NAME)
     @EmbeddedId
-    private Key key = new Key();
+    @GenericGenerator(name = GENERATOR_NAME, strategy = "com.ote.test.model.Person$KeyGenerator")
+    @GeneratedValue(generator = GENERATOR_NAME)
+    private Key key;
 
     @Embeddable
     @Data
@@ -26,9 +34,9 @@ public class Person implements IEntity<Person.Key> {
     @NoArgsConstructor
     public static class Key implements IEntity.Key {
 
-        @Column(name = "ID")
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "ID", nullable = false)
         private Integer id;
+
     }
 
     @Column(name = "FIRST_NAME")
@@ -36,4 +44,43 @@ public class Person implements IEntity<Person.Key> {
 
     @Column(name = "LAST_NAME")
     private String lastName;
+
+    public static class KeyGenerator implements IdentifierGenerator {
+
+        @Override
+        public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
+
+            if (!(object instanceof Person)) {
+                return null;
+            }
+
+            Person entity = (Person) object;
+            Person.Key id = entity.getKey();
+
+            if (id == null) {
+                id = new Person.Key();
+            }
+
+            if (id.getId() == null) {
+                id.setId(getNextId(session));
+            }
+
+            return id;
+        }
+
+        private int getNextId(SessionImplementor session) {
+
+            Connection connection = session.connection();
+            try {
+                PreparedStatement ps = connection.prepareStatement("SELECT S_PERSON_INFO.nextval from dual");
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+            throw new IllegalStateException("Can't get id from sequence");
+        }
+    }
 }
