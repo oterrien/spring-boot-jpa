37e4cac57c801123c6f936e71aa2f6dd6d695c06
Olivier TERRIEN <oterrien@neuf.fr>
2016-11-14 12:41:23 +0100

    Update PersonController.java

--- src/main/java/com/ote/test/controller/PersonController.java
+++ src/main/java/com/ote/test/controller/PersonController.java
@@ -5,7 +5,6 @@ import com.ote.test.model.Person;
 import com.ote.test.model.PersonParameter;
 import com.ote.test.service.persistence.IEntityPersistenceService;
 import com.ote.test.service.persistence.IEntityPersistenceService.Result;
-import com.ote.test.service.persistence.IEntityPersistenceService.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,23 +13,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-import java.util.function.Supplier;
-
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/persons")
 @Slf4j
-public class PersonController {
+public class PersonController extends EntityController<Person, Person.Key>{
 
-    @Autowired
-    private IEntityPersistenceService<Person, Person.Key> personPersistenceService;
+    public PersonController(@Autowired IEntityPersistenceService<Person, Person.Key> personPersistenceService){
+        super(personPersistenceService);
+    }
 
     @Traceable
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<Page<Person>> getAll(PersonParameter parameter, Pageable pageRequest) {
+    public ResponseEntity<Page<Person>> getMany(PersonParameter parameter, Pageable pageRequest) {
 
-        return get(() -> personPersistenceService.findAll(parameter, pageRequest));
+        return getMany(() -> entityPersistenceService.findMany(parameter, pageRequest));
     }
 
     @Traceable
@@ -38,42 +35,21 @@ public class PersonController {
     @ResponseBody
     public ResponseEntity<Person> get(@PathVariable(name = "id") Integer id) {
 
-        return get(() -> personPersistenceService.findOne(new Person.Key(id)));
-    }
-
-    private <T> ResponseEntity<T> get(Supplier<Optional<T>> supplier) {
-
-        Optional<T> result = supplier.get();
-
-        return result.isPresent() ?
-                new ResponseEntity<>(result.get(), HttpStatus.FOUND) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return getOne(() -> entityPersistenceService.findOne(new Person.Key(id)));
     }
 
     @Traceable
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable(name = "id") Integer id) {
 
-        return delete(() -> personPersistenceService.delete(new Person.Key(id)));
+        return delete(() -> entityPersistenceService.delete(new Person.Key(id)));
     }
 
     @Traceable
     @RequestMapping(method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteAll() {
 
-        return delete(() -> personPersistenceService.deleteAll());
-    }
-
-    private ResponseEntity<Void> delete(Supplier<Status> supplier) {
-
-        Status status = supplier.get();
-
-        switch (status) {
-            case DELETED:
-                return new ResponseEntity<>((Void) null, HttpStatus.NO_CONTENT);
-            default:
-                return new ResponseEntity<>((Void) null, HttpStatus.NOT_FOUND);
-        }
+        return delete(() -> entityPersistenceService.deleteAll());
     }
 
     @Traceable
@@ -81,8 +57,7 @@ public class PersonController {
     @ResponseBody
     public ResponseEntity<Person> create(@RequestBody Person person) {
 
-        //person.setKey(new Person.Key());
-        Result<Person> result = personPersistenceService.create(person);
+        Result<Person> result = entityPersistenceService.create(person);
 
         switch (result.getStatus()) {
             case CREATED:
@@ -100,7 +75,7 @@ public class PersonController {
 
         return updateOrPatch(() -> {
             person.setKey(new Person.Key(id));
-            return personPersistenceService.update(person);
+            return entityPersistenceService.update(person);
         });
     }
 
@@ -112,21 +87,7 @@ public class PersonController {
 
         return updateOrPatch(() -> {
             person.setKey(new Person.Key(id));
-            return personPersistenceService.partialUpdate(person);
+            return entityPersistenceService.partialUpdate(person);
         });
-    }
-
-    private ResponseEntity<Person> updateOrPatch(Supplier<Result<Person>> supplier) {
-
-        Result<Person> result = supplier.get();
-
-        switch (result.getStatus()) {
-            case UPDATED:
-                return new ResponseEntity<>(result.getEntity(), HttpStatus.OK);
-            case NOT_FOUND:
-                return new ResponseEntity<>((Person) null, HttpStatus.NOT_FOUND);
-            default:
-                return new ResponseEntity<>((Person) null, HttpStatus.BAD_REQUEST);
-        }
     }
 }
